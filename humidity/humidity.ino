17179172407ba6048f828e69992fb7ec35fd86cd
Matias Gutierrez <gutierrez.matias@outlook.com>
2019-02-16 15:10:18 -0300

    two humidity sensors working with servo

--- humidity/humidity.ino
+++ humidity/humidity.ino
@@ -1,13 +1,54 @@
-void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+
+#include <Servo.h> 
+
+// humidity
+int sensor_pin = A0; 
+int sensor_pin2 = A3;
+int output_value;
+int output_value2;
+
+//servo
+int plant2 = 0;
+int plant1 = 180;
+int servoPin = 13;
+Servo motor;
+
+void moveTo(int plant) {
+  motor.write(plant);
 }
 
-// the loop routine runs over and over again forever:
+void setup() {
+  motor.attach(servoPin);
+  Serial.begin(9600);
+  Serial.println("Reading From the Sensor ...");
+  delay(2000);
+}
+
 void loop() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
-  // print out the value you read:
-  Serial.println(sensorValue);
-  delay(1);        // delay in between reads for stability
+  output_value = analogRead(sensor_pin);
+  output_value = map(output_value,550,0,0,100);
+  Serial.print("Plant 1: ");
+  Serial.print(output_value < 0 ? 0 : output_value);
+  Serial.println("%");
+
+  if (output_value < 40){
+    moveTo(plant1);
+    Serial.println("Moving to plant 1");
+  }
+  delay(2000);
+
+
+  output_value2 = analogRead(sensor_pin2);
+  output_value2 = map(output_value2,550,0,0,100);
+  Serial.print("Plant 2: ");
+  Serial.print(output_value2 < 0 ? 0 : output_value2);
+  Serial.println("%");
+
+  if (output_value2 < 40){
+    moveTo(plant2);
+    Serial.println("Moving to plant 2");
+  }
+  delay(2000);
+
+  Serial.println("--------");
 }
